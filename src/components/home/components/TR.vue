d2b5f05ade637c8cba17a6b07931d707b65cd3b2
SaMeTa-cxl <903412026@qq.com>
2024-04-12 19:10:23 +0800

    更新了TR（任务审核）模块，接入了接口，并经过了mock测试

--- src/components/home/components/TR.vue
+++ src/components/home/components/TR.vue
@@ -1,47 +1,117 @@
 <template>
-  <el-card class="box-card">
-    <div slot="header" class="clearfix">
-      <span>任务审核</span>
-      <el-button style="float: right; padding: 3px 0" type="primary">通过</el-button>
-      <el-button style="float: right; padding: 3px 0; margin: 0 20px" type="danger">不通过</el-button>
-    </div>
-    <div v-for="task in taskArray" :key="task.task_id" class="text item">
-      <el-descriptions title="任务信息">
-        <!-- <template slot="extra">
-          <el-button type="primary" size="small">更多</el-button>
-        </template> -->
-        <el-descriptions-item label="标题"> {{ task.task_title }} </el-descriptions-item>
-        <el-descriptions-item label="任务地点"> {{ task.task_location }} </el-descriptions-item>
-        <el-descriptions-item label="任务时间币数"> {{ task.task_coin}} </el-descriptions-item>
-        <el-descriptions-item label="任务发布时间"> {{new Date(task.task_begintime).toLocaleString()}} </el-descriptions-item>
-        <el-descriptions-item label="任务截止时间"> {{new Date(task.task_endtime).toLocaleString()}}</el-descriptions-item>
-        <el-descriptions-item label="任务发布者编号"> {{task.task_publisherId}}</el-descriptions-item>
-        <el-descriptions-item label="任务发布者名字"> {{task.task_publisherName}}</el-descriptions-item>
-        <el-descriptions-item label="正文"> {{task.task_text}}</el-descriptions-item>
-      </el-descriptions>
-    </div>
-  </el-card>
+  <div>
+    <el-card v-for="task in taskArray" :key="task.task_id" class="box-card" style="margin: 10px 0">
+      <div slot="header" class="clearfix">
+        <span>任务审核</span>
+        <el-button style="float: right; padding: 3px 3px" type="primary" @click="onAccept(task.task_id)">通过</el-button>
+        <el-button style="float: right; padding: 3px 3px; margin: 0 20px" type="danger" @click="onReject(task.task_id)">拒绝</el-button>
+      </div>
+      <div>
+        <el-descriptions title="任务信息">
+          <el-descriptions-item label="标题"> {{ task.task_title }} </el-descriptions-item>
+          <el-descriptions-item label="任务地点"> {{ task.task_location }} </el-descriptions-item>
+          <el-descriptions-item label="任务时间币数"> {{ task.task_coin}} </el-descriptions-item>
+          <el-descriptions-item label="任务发布时间"> {{new Date(task.task_begintime).toLocaleString()}} </el-descriptions-item>
+          <el-descriptions-item label="任务截止时间"> {{new Date(task.task_endtime).toLocaleString()}}</el-descriptions-item>
+          <el-descriptions-item label="任务发布者编号"> {{task.task_publisherId}}</el-descriptions-item>
+          <el-descriptions-item label="任务发布者名字"> {{task.task_publisherName}}</el-descriptions-item>
+          <el-descriptions-item label="正文"> {{task.task_text}}</el-descriptions-item>
+        </el-descriptions>
+      </div>
+    </el-card>
+  </div>
 </template>
 <script>
+
+import axios from 'axios';
+
 export default {
   name: 'TR',
   data() {
     return {
-      taskArray: [
-        {
-          "task_id": 71,
-          "task_publisherName": "何看土极",
-          "task_publisherId": 83,
-          "task_coin": 92,
-          "task_title": "口情改参细律无",
-          "task_text": "laborum eiusmod nostrud",
-          "task_location": "irure",
-          "task_begintime": 90013813070,
-          "task_endtime": 1445567805053
-        }
-      ]
+      taskArray: []
     }
   },
+  methods: {
+    onAccept(id) {
+      console.log("Task accepted!");
+
+      axios({
+        method: 'post',
+        url: 'https://mock.apifox.com/m1/4278752-3920807-default/taskReview/review',
+        data: JSON.stringify({
+          token: localStorage.getItem('token'),
+          task_id: id,
+          reviewStatus: '2',
+          reviewAdvice: null
+        })
+      }).then( response => {
+        if(response.data['status']) {
+          this.$message({
+            message: '审核成功',
+            type: 'success'
+          });
+          this.taskArray = this.taskArray.filter(item => item.task_id != id);
+        }
+        else 
+          this.$message.error('审核失败：'+response.data['msg']);
+      })
+    },
+    async onReject(id) {
+      console.log("Task Rejected!");
+      let flag = false;
+      let advice = '';
+      await this.$prompt('请输入拒绝理由', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+      }).then(({ value }) => {
+        flag = true;
+        advice = value;
+        console.log(value);
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消'
+        });       
+      });
+
+      if (flag) {
+        axios({
+          method: 'post',
+          url: 'https://mock.apifox.com/m1/4278752-3920807-default/taskReview/review',
+          data: JSON.stringify({
+            token: localStorage.getItem('token'),
+            task_id: id,
+            reviewStatus: '1',
+            reviewAdvice: advice
+          })
+        }).then( response => {
+          if(response.data['status']) {
+            this.$message({
+              message: '审核成功',
+              type: 'success'
+            });
+            this.taskArray = this.taskArray.filter(item => item.task_id != id);
+          }
+          else 
+            this.$message.error('审核失败：'+response.data['msg']);
+        })
+      }
+    }
+  },
+  created() {
+    axios({
+      method: 'get',
+      url: 'https://mock.apifox.com/m1/4278752-3920807-default/taskReview/get',
+      data: JSON.stringify({token: localStorage.getItem('token')})
+    }).then( response => {
+      this.taskArray = response.data.taskArray;
+    }).catch( err => {
+      console.log(err);
+      this.$router.push('/');
+      localStorage.removeItem('token');
+    })
+  }
 }
 </script>
 <style>
